644e17c87500d62594844425c7da276a8c278d7f
ilahyani <lahyani799@gmail.com>
2023-10-13 01:46:30 +0100

    wrap resquest in try catch block

--- frontend/app/auth/42-redirect/page.tsx
+++ frontend/app/auth/42-redirect/page.tsx
@@ -4,22 +4,26 @@ import { User } from "../../../types";
 
 async function getPreAuthData() {
   const cookie = cookies();
-  const response = await fetch("http://localhost:3000/auth/preAuthData", {
-    credentials: "include",
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-      Accept: "application/json",
-      Cookie: cookies()
-        .getAll()
-        .map(({ name, value }) => `${name}=${value}`)
-        .join("; "),
-    },
-  });
-  if (response.ok) {
-    const res = await response.json();
-    return res.user;
-  } else alert("Failed to fetch user data");
+  try {
+    const response = await fetch("http://localhost:3000/auth/preAuthData", {
+      credentials: "include",
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+        Cookie: cookies()
+          .getAll()
+          .map(({ name, value }) => `${name}=${value}`)
+          .join("; "),
+      },
+    });
+    if (response.ok) {
+      const res = await response.json();
+      return res.user;
+    } else console.log("Failed to fetch user data");
+  } catch (e) {
+    console.log(e);
+  }
 }
 
 export default async function ApiRedirectPage() {
