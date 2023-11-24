3f9aeea030c7aae46ee1d885f8ab88f0e941b739
Yahya Amzil <yamzil@e3r2p16.1337.ma>
2023-11-24 18:38:59 +0100

    working in ball's mouvement

--- backend/src/game/game.gateway.ts
+++ backend/src/game/game.gateway.ts
@@ -9,23 +9,33 @@ import { PrismaService } from '../prisma/prisma.service';
 import { GameService } from './game.service';
 import { Server, Socket } from 'socket.io';
 import { GameType } from '@prisma/client';
+import * as _ from "lodash";
 
 @WebSocketGateway({
 	cors: '*',
 	namespace: 'game',
 })
 export class GameGateway implements OnGatewayConnection, OnGatewayDisconnect {
-	gameQueue: any[] = [];
-	mapPlayers: Map<string, any> = new Map();
 	canvasWidth = 1080;
 	canvasHeight = 720;
+	ball = {
+		x: this.canvasWidth / 2,
+		y: this.canvasHeight / 2,
+		radius: 10,
+		velocityX: 5,
+		velocityY: 5,
+		speed: 7,
+	}
+	gameQueue: any[] = [];
+	mapPlayers: Map<string, any> = new Map();
 	mapSocketToPlayer: Map<string, Socket> = new Map();
+	roomName: string;
 	@WebSocketServer() server: Server;
 	constructor(
 		private readonly GameRoom: GameService,
 		private readonly prisma: PrismaService,
 	) {
-		// this.startInterval();
+		this.startInterval();
 	}
 
 	handleConnection(client: Socket, ...args: any[]) {
@@ -33,6 +43,7 @@ export class GameGateway implements OnGatewayConnection, OnGatewayDisconnect {
 		if (playeId) {
 			this.mapSocketToPlayer.set(playeId, client);
 		}
+		// console.log('Player connected: ', client.id);
 	}
 
 	@SubscribeMessage('RandomMatch')
@@ -46,25 +57,19 @@ export class GameGateway implements OnGatewayConnection, OnGatewayDisconnect {
 				y: this.canvasHeight / 2 - 50,
 				score: 0,
 				width: 20,
-				height: 200,
+				height: 150,
 			};
 			if (!this.gameQueue.some(player => player.id === ObjectPlayer.id)) {
 				this.gameQueue.push(ObjectPlayer);
-				// console.log(this.gameQueue);
 			}
 			if (this.gameQueue.length >= 2) {
-				// console.log(' before', this.gameQueue);
 				const player = this.gameQueue.shift(); // player 1
-				// console.log(player.id);
 				const opponent = this.gameQueue.shift(); // player 2
-				// console.log(opponent.id);
-				// console.log(' after', this.gameQueue);
-				// console.log('len', this.gameQueue.length);
 				opponent.x = this.canvasWidth - 30;
-				const room = this.GameRoom.createRoom();
+				this.roomName = this.GameRoom.createRoom();
 				const match = await this.prisma.game.create({
 					data: {
-						id: room,
+						id: this.roomName,
 						Player: {
 							connect: { id: player.id },
 						},
@@ -76,14 +81,14 @@ export class GameGateway implements OnGatewayConnection, OnGatewayDisconnect {
 						type: GameType.RandomMatch,
 					},
 				});
-				player.socket.join(room);
-				opponent.socket.join(room);
+				player.socket.join(this.roomName);
+				opponent.socket.join(this.roomName);
 				this.mapPlayers.set(player.id, player);
 				this.mapPlayers.set(opponent.id, opponent);
-				this.server.to(room).emit('RandomMatch', {
+				this.server.to(this.roomName).emit('RandomMatch', {
 					player: player.id, opponent: opponent.id,
 					playerY: player.y, opponentY: opponent.y,
-					room: room,
+					room: this.roomName, playerScore: player.score, opponentScore: opponent.score,
 				});
 			}
 		}
@@ -94,29 +99,77 @@ export class GameGateway implements OnGatewayConnection, OnGatewayDisconnect {
 
 	@SubscribeMessage('move')
 	movePaddle(socket: Socket, payload: any): void {
-		console.log(payload);
 		if (this.mapPlayers.has(payload.player)) {
 			const player = this.mapPlayers.get(payload.player);
 			if (payload.direction === 'up') {
-				player.y -= 7;
+				player.y -= 50;
 			}
 			else if (payload.direction === 'down') {
-				player.y += 7;
+				player.y += 50;
+			}
+			if (player.y < 0) {
+				player.y = 0;
+			}
+			else if (player.y > this.canvasHeight - player.height) {
+				player.y = this.canvasHeight - player.height;
 			}
 			this.server.to(payload.room).emit('PlayerMoved', { player: player.id, y: player.y });
 		}
 	}
 
+	moveBall(): void {
+		if (this.mapPlayers.size < 2) return;
+		const player = _.take(Array.from(this.mapPlayers.values()), 1)[0];
+		const opponent = _.takeRight(Array.from(this.mapPlayers.values()), 1)[0];
+		// let ball = {
+		// 	x: this.canvasWidth / 2,
+		// 	y: this.canvasHeight / 2,
+		// 	radius: 10,
+		// 	velocityX: 5,
+		// 	velocityY: 5,
+		// 	speed: 7,
+		// }
+		// console.log(ball.x, ball.y);
+		this.ball.x += this.ball.velocityX;
+		this.ball.y += this.ball.velocityY;
+		if (this.ball.y + this.ball.radius > this.canvasHeight || this.ball.y - this.ball.radius < 0)
+		this.ball.velocityY = -this.ball.velocityY;
+		let playerdetected = (this.ball.x < this.canvasWidth / 2) ? player : opponent;
 
-	// moveBall(): void {
+		const collision = (ball: any, player: any) => {
+			player.top = player.y;
+			player.right = player.x + player.width;
+			player.bottom = player.y + player.height;
+			player.left = player.x;
+			ball.top = ball.y - ball.radius;
+			ball.right = ball.x + ball.radius;
+			ball.bottom = ball.y + ball.radius;
+			ball.left = ball.x - ball.radius;
+			return ball.left < player.right && ball.top < player.bottom && ball.right > player.left && ball.bottom > player.top;
+		}
+		if (collision(this.ball, playerdetected)) {
+			let collidePoint = (this.ball.y - (playerdetected.y + playerdetected.height / 2));
+			collidePoint = collidePoint / (playerdetected.height / 2);
+			let angleRad = (Math.PI / 4) * collidePoint;
+			let direction = (this.ball.x + this.ball.radius < this.canvasWidth / 2) ? 1 : -1;
+			this.ball.velocityX = direction * this.ball.speed * Math.cos(angleRad);
+			this.ball.velocityY = this.ball.speed * Math.sin(angleRad);
+			this.ball.speed += 0.1;
+		}
+		if (this.ball.x + this.ball.radius > this.canvasWidth) {
+			this.ball.velocityX = -this.ball.velocityX;
+		}
+		else if (this.ball.x - this.ball.radius < 0) {
+			this.ball.velocityX = -this.ball.velocityX;
+		}
+		this.server.to(this.roomName).emit('BallMoved', { x: this.ball.x, y: this.ball.y });
+	}
 
-	// }
-
-	// startInterval() {
-	// 	setInterval(() => {
-
-	// 	}, 10);
-	// }
+	startInterval() {
+		setInterval(() => {
+			this.moveBall();
+		}, 30);
+	}
 
 	handleDisconnect(socket: Socket): void {
 		const playeId: string = socket.handshake.auth.token;
@@ -124,7 +177,7 @@ export class GameGateway implements OnGatewayConnection, OnGatewayDisconnect {
 			this.mapSocketToPlayer.delete(playeId);
 			this.gameQueue = this.gameQueue.filter(player => player.id !== playeId);
 
-			console.log('Player disconnected: ', socket.id);
+			// console.log('Player disconnected: ', socket.id);
 		}
 	}
 
--- frontend/components/Game/RandomGame.tsx
+++ frontend/components/Game/RandomGame.tsx
@@ -1,5 +1,4 @@
 'use client'
-import PingPong from "./PingPong";
 import { useEffect, useRef, useState, useCallback, use } from "react";
 import { Socket, io } from "socket.io-client";
 import cookie from 'js-cookie';
@@ -8,6 +7,7 @@ import { Player, Net } from '../../types';
 
 const canvasWidth = 1080;
 const canvasHeight = 720;
+
 const drawTable = (context: any, canvas: any, x: number, y: number, color: string) => {
 	context.fillStyle = 'black';
 	context.fillRect(0, 0, canvasWidth, canvasHeight);
@@ -25,7 +25,7 @@ const drawBall = (context: any, x: number, y: number, color: string) => {
 // draw the rectangle aka the paddle
 const drawRect = (context: any, x: number, y: number, width: number, height: number, color: string) => {
 	context.beginPath();
-	context.rect(x, y, width, height);
+	context.roundRect(x, y, width, height, 10);
 	context.fillStyle = color;
 	context.fill();
 	context.closePath();
@@ -33,7 +33,7 @@ const drawRect = (context: any, x: number, y: number, width: number, height: num
 
 // draw the net
 const drawNet = (context: any, canvas: any, x: number, y: number, width: number, height: number, color: string) => {
-	for (let i = 0; i <= canvasHeight; i += 25) {
+	for (let i = 0; i <= canvasHeight; i += 20) {
 		drawRect(context, x, y + i, width, height, color);
 	}
 }
@@ -41,31 +41,31 @@ const drawNet = (context: any, canvas: any, x: number, y: number, width: number,
 let net: Net = {
 	x: canvasWidth / 2 - 2 / 2,
 	y: 0,
-	width: 7,
+	width: 5,
 	height: 10,
 	color: 'white'
 }
 
 export default function RandomMatch() {
+    const canvasRef = useRef<HTMLCanvasElement>(null);
     const [socket, setSocket] = useState<Socket>();
     const [playerY, setPlayerY] = useState( canvasHeight / 2 - 50 );
     const [openentY, setOpenentY] = useState(  canvasHeight / 2 - 50 );
-    let room = '';
-    const ball = { x: canvasWidth / 2, y: canvasHeight / 2, };
-	const Player = {  x: 10, y: canvasHeight / 2 - 50, width: 20, height: 150, color: 'white', score: 0 };
+	const Player = { x: 10, y: canvasHeight / 2 - 50, width: 20, height: 150, color: 'white', score: 0 };
 	const Opponent= { x: canvasWidth - 30, y: canvasHeight / 2 - 50, width: 20, height: 150, color: 'white', score: 0 };
-	const canvasRef = useRef<HTMLCanvasElement>(null);
 	const [ballMoving, setBall] = useState(false);
+    const [ballY, setBallY] = useState(canvasHeight / 2);
+    const [ballX, setBallX] = useState(canvasWidth  / 2);
+    let room = '';
 
     const render = () =>  {
 		const canvas = canvasRef.current;
-		let context = canvas?.getContext('2d');
+		const context = canvas?.getContext('2d');
 		drawTable(context, canvas, canvasHeight, canvasWidth, 'black');
-		drawBall(context, ball.x, ball.y, 'white');
+		drawBall(context, ballX, ballY, 'white');
 		drawRect(context, Player.x, playerY, Player.width, Player.height, Player.color);
 		drawRect(context, Opponent.x, openentY, Opponent.width, Opponent.height, Opponent.color);
 		drawNet(context, canvas, net.x, net.y, net.width, net.height, net.color);
-        console.log(openentY, playerY)
 	};
 	
 	const onCountdownEnd = () => {
@@ -73,17 +73,17 @@ export default function RandomMatch() {
 	};
 
 	useEffect(() => {
-			const gameLoop = () => {
-				render();
-				// requestAnimationFrame(gameLoop);
-			}
-			gameLoop();
-	}, [playerY, openentY]);
+        const gameLoop = () => {
+            setPlayerY(preV => preV + (playerY - preV) * 0.6);
+            setOpenentY(preV => preV + (openentY - preV) * 0.6);
+            render();
+        }
+        gameLoop();
+	}, [playerY, openentY, ballMoving]);
 	
 
     const keyPress = (e: any) => {
         if (e.keyCode === 38 ) {
-            console.log(socket);
             socket?.emit('move', { player: cookie.get('USER_ID'), direction: 'up', room: room });
         }
         else if (e.keyCode === 40 ) {
@@ -98,26 +98,23 @@ export default function RandomMatch() {
             }
         });
         setSocket(socketIo);
-        function cleanup() {
+        return () => { 
             window?.removeEventListener('keydown', keyPress);
             socketIo.disconnect();
         }
-        return cleanup;
     }, []);
 
     useEffect(() => {
         if (!socket) return;
         window?.addEventListener('keydown', keyPress);
-        socket.emit('RandomMatch', { data: 'hello' });
-        function cleanup() {
+        socket.emit('RandomMatch', { player: cookie.get('USER_ID'), room: room });
+        return () => {
             socket?.disconnect();
-        }
-        return cleanup;
+        };
     }, [socket]);
 
     useEffect(() => {
         if (socket) {
- /// TODO: fix this
             socket.on('RandomMatch', (data: any) => {
                 if (data.player === cookie.get('USER_ID')) {
                     setPlayerY( data.playerY );
@@ -138,10 +135,13 @@ export default function RandomMatch() {
                     setOpenentY(data.y);
                 }
             });
+            socket.on('BallMoved', (data: any) => {
+                setBallX(data.x);
+                setBallY(data.y);
+            });
         }
-        return () => {
-        };
-    }, [socket, playerY, openentY]);
+    }, [socket, playerY, openentY, ballMoving, ballX, ballY]);
+
     return (
 		<div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
 			{ballMoving ? null : <Countdown onCountdownEnd={onCountdownEnd} />}
@@ -149,3 +149,6 @@ export default function RandomMatch() {
 		</div>
 	)
 }
+
+
+// 545 365
