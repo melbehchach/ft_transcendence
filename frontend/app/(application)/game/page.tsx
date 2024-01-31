a68d8b17565ee436b7198ac9c5927e231c82b087
ilahyani <lahyani799@gmail.com>
2024-01-31 22:53:03 +0100

    responsivity fix

--- frontend/app/(application)/game/page.tsx
+++ frontend/app/(application)/game/page.tsx
@@ -6,67 +6,49 @@ export default function GamePage() {
   const router = useRouter();
   return (
     <>
-      <div className="bg-background w-full h-vh flex px-5 py-10 justify-center ">
-        <div className="flex justify-center gap-5 xl:gap-10 ">
-          <div className="m-auto">
-            <h1 className="text-text font-bold text-3xl md:text-5xl lg:text-7xl xl:text-8xl min-[2560px]:text-[10rem] text-center">
+      <div className="bg-background w-full h-screen flex px-5 py-10 justify-center ">
+        <div className="flex justify-center content-center gap-5 w-full">
+          <div className="w-[80%] flex flex-col md:place-content-evenly">
+            <h1 className="text-text font-bold text-3xl md:text-5xl min-[2000px]:text-7xl text-center">
               READY TO PONG?
             </h1>
-            <div className="py-5 text-small xl:text-body min-[2560px]:text-title leading-tight text-center text-textSecondary">
+            <div className="py-5 text-small lg:text-body leading-tight text-center text-textSecondary">
               {
                 "You can challenge your friends or find random opponents online. Let's GO !"
               }
             </div>
-            <div className="w-full gap-2 flex flex-col md:flex-row md:gap-6 justify-center">
-              <div className="w-full gap-2 md:gap-4 flex flex-col">
-                <div className="text-text text-body text-center min-[2560px]:text-title">
-                  Find a random opponent
+            <div className="w-full h-full gap-2 flex flex-col md:flex-row md:gap-6 justify-center">
+              <div className="w-full h-full gap-2 md:gap-4 flex flex-col">
+                <div className="text-text text-body min-[2000px]:text-title text-center">
+                  Random opponent
                 </div>
                 <div className="text-small leading-tight text-center md:hidden text-textSecondary">
-                  {"Let us find you a random opponent"}
+                  {"Find a random opponent"}
                 </div>
-                <div>
-                  <Image
-                    className="rounded-lg"
-                    src="/img/random.png"
-                    layout="responsive"
-                    width={100}
-                    height={100}
-                    alt="Challenge Friends"
-                  />
-                </div>
-                <div className="text-small leading-tight text-center hidden md:block xl:text-body min-[2560px]:text-title text-textSecondary">
-                  {"Let us find you a random opponent"}
+                <div className="h-full w-full bg-[url('/img/random.png')] bg-no-repeat bg-center bg-cover"></div>
+                <div className="text-small min-[2000px]:text-body leading-tight text-center hidden md:block text-textSecondary">
+                  {"Find a random opponent"}
                 </div>
                 <button
-                  className="block bg-primary text-text text-body px-6 py-3 rounded-lg xl:rounded-full w-auto mb-10"
+                  className="block bg-primary text-text text-body px-6 py-3 rounded-lg min-[2000px]:rounded-full w-auto mb-10 md:mb-0"
                   onClick={() => router.push("/game/random")}
                 >
                   Find Opponent
                 </button>
               </div>
-              <div className="w-full gap-2 md:gap-4 flex flex-col">
-                <div className="text-text text-body text-center min-[2560px]:text-title">
+              <div className="w-full h-full gap-2 md:gap-4 flex flex-col">
+                <div className="text-text text-body min-[2000px]:text-title text-center">
                   Challenge a friend
                 </div>
                 <div className="text-small leading-tight text-center md:hidden text-textSecondary">
                   {"challenge your friends for a 1v1"}
                 </div>
-                <div>
-                  <Image
-                    className="rounded-lg"
-                    layout="responsive"
-                    width={100}
-                    height={100}
-                    src="/img/friend.png"
-                    alt="Challenge Friends"
-                  />
-                </div>
-                <div className="text-small leading-tight text-center hidden md:block xl:text-body min-[2560px]:text-title text-textSecondary">
+                <div className="w-full h-full bg-[url('/img/friend.png')] bg-no-repeat bg-center bg-cover"></div>
+                <div className="text-small min-[2000px]:text-body leading-tight text-center hidden md:block text-textSecondary">
                   {"challenge your friends for a 1v1"}
                 </div>
                 <button
-                  className="block bg-primary text-text text-body px-6 py-3 rounded-lg xl:rounded-full w-auto"
+                  className="block bg-primary text-text text-body px-6 py-3 rounded-lg min-[2000px]:rounded-full w-auto"
                   onClick={() => router.push("/game/friend")}
                 >
                   Challenge a Friend
@@ -74,16 +56,7 @@ export default function GamePage() {
               </div>
             </div>
           </div>
-          <div className="hidden xl:flex m-auto w-full justify-center">
-            <Image
-              className="rounded-lg"
-              layout="responsive"
-              width={100}
-              height={100}
-              src="/img/GameIntro.png"
-              alt="Game Intro Image"
-            />
-          </div>
+          <div className="hidden 2xl:block m-auto w-full h-full bg-[url('/img/GameIntro.png')] bg-no-repeat bg-center bg-cover" />
         </div>
       </div>
     </>
