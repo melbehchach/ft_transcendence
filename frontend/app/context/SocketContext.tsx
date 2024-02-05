f573f84105ebd263d9215eecea559fe5d126afb4
aeddaqqa <achrafeddaqqaq@gmail.com>
2024-02-05 17:31:05 +0100

    add socket context

---
+++ frontend/app/context/SocketContext.tsx
@@ -0,0 +1,61 @@
+"use client";
+import Cookies from "js-cookie";
+import { createContext, useContext, useEffect, useState } from "react";
+import { Socket, io } from "socket.io-client";
+
+const SocketContext = createContext(null);
+
+const SocketContextProvider = ({ children }) => {
+  const [socket, setSocket] = useState(null);
+
+  useEffect(() => {
+    const newSocket: Socket = io("http://localhost:3000/game", {
+      auth: {
+        token: Cookies.get("USER_ID"),
+      },
+    });
+    setSocket(newSocket);
+
+    return () => {
+      newSocket.disconnect();
+    };
+  }, []);
+
+  const sendMessage = (message) => {
+    if (socket) {
+      socket.emit("chat message", message);
+    }
+  };
+
+  const joinRoom = (roomName) => {
+    if (socket) {
+      socket.emit("join room", roomName);
+    }
+  };
+
+  const leaveRoom = (roomName) => {
+    if (socket) {
+      socket.emit("leave room", roomName);
+    }
+  };
+
+  return (
+    <SocketContext.Provider
+      value={{ socket, sendMessage, joinRoom, leaveRoom }}
+    >
+      {children}
+    </SocketContext.Provider>
+  );
+};
+
+export const useSocket = () => {
+  const context = useContext(SocketContext);
+
+  if (!context) {
+    throw new Error("");
+  }
+
+  return context;
+};
+
+export default SocketContextProvider;
