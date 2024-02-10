24e89a92adc9b483f74cc06f15991ae0ac7ad602
ilahyani <lahyani799@gmail.com>
2024-02-10 16:13:16 +0100

    avatar upload bug fix

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -57,7 +57,6 @@ export class AuthController {
     );
     res.cookie('JWT_TOKEN', accessToken);
     res.cookie('USER_ID', id);
-    res.cookie('USER', '', { expires: new Date() });
     return { msg: 'Success' };
   }
 
